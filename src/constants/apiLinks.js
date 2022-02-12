664bb0d3546b98c8c436e99f40c885b89b63717c
Alex <itgtktdbx@gmail.com>
2022-02-12 21:28:59 +0300

    feat: add endpoints for users and signin

--- src/constants/apiLinks.js
+++ src/constants/apiLinks.js
@@ -1,2 +1,4 @@
 export const BASE_LINK = 'https://rslang-minsk-grodno.herokuapp.com';
 export const WORDS_LINK = `${BASE_LINK}/words`;
+export const USERS_LINK = `${BASE_LINK}/users`;
+export const SIGNIN_LINK = `${BASE_LINK}/signin`;
