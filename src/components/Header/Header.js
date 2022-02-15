b27e07bc7061291e8e0b4d97a647cb2e843540f0
Kirill Litvin <litvin.kirill.m@mail.ru>
2022-02-16 00:51:23 +0300

    fix: style for header button

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,20 @@
 import React, { Fragment } from 'react';
 import './styles.css';
 import { NavMenu } from '../Header/HeaderNav';
+import { LoginForm } from '../LoginForm/LoginForm';
 
 export function Header() {
+  function showLoginForm() {
+    <LoginForm />;
+  }
   return (
     <>
       <header className="header">
-        <div className="header_wrap">
-          <div className="header_logo"></div>
-          <NavMenu />
-          <button className="autorisation">
-            <p className="login_text">ВОЙТИ</p>
-          </button>
-        </div>
+        <div className="header_logo"></div>
+        <NavMenu />
+        <button className="autorisation" onClick={showLoginForm}>
+          <p className="login_text">Войти</p>
+        </button>
       </header>
     </>
   );
