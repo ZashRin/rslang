c97365f8881669eeeacab1fff4e2a3783d1fdba9
Kirill Litvin <litvin.kirill.m@mail.ru>
2022-02-15 22:55:58 +0300

    feat: add logo in header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,11 +7,7 @@ export function Header() {
     <>
       <header className="header">
         <div className="header_wrap">
-          <div className="header_logo">
-            <a className="header_logo_link" href="!">
-              RSLang
-            </a>
-          </div>
+          <div className="header_logo"></div>
           <NavMenu />
           <button className="autorisation">
             <p className="login_text">ВОЙТИ</p>
