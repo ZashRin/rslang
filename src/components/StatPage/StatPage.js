d74b9af31a8a0f74801acb6ef44bb0fc6b2acb37
Alex <itgtktdbx@gmail.com>
2022-02-19 22:47:53 +0300

    fix: fix callback loop

--- src/components/StatPage/StatPage.js
+++ src/components/StatPage/StatPage.js
@@ -1,22 +1,31 @@
-import React, { useCallback, useContext, useRef } from 'react';
+import React, { useCallback, useContext, useEffect, useRef } from 'react';
 import { Context } from '../../Context/Context';
 import { getUserSettings, getUserStat, updateUser, updateUserSettings, updateUserStat } from '../../utils/api';
 import { clearStorage } from '../../utils/storage';
 
 export function StatPage() {
   const [context, setContext] = useContext(Context);
+  const { id, token, authenticated } = context;
   const getStat = useCallback(async () => {
-    const userLearnedWords = await getUserStat(context.id, context.token);
-    //console.log(userLearnedWords.learnedWords);
-    const userWordsPerDay = await getUserSettings(context.id, context.token);
-    setContext({ ...context, learned: userLearnedWords.learnedWords, wordsPerDay: userWordsPerDay.wordsPerDay });
-  }, [context, setContext]);
+    const userLearnedWords = await getUserStat(id, token);
+    const userWordsPerDay = await getUserSettings(id, token);
+    setContext((prevContext) => ({
+      ...prevContext,
+      learned: userLearnedWords.learnedWords,
+      wordsPerDay: userWordsPerDay.wordsPerDay,
+    }));
+  }, [id, token, setContext]);
   // eslint-disable-next-line no-unused-vars
   const setStat = useCallback(async () => {
     await updateUserStat(context.id, context.token, 3);
     await updateUserSettings(context.id, context.token, 1);
   }, [context.id, context.token]);
-  getStat();
+  useEffect(() => {
+    if (authenticated) {
+      getStat();
+    }
+  }, [authenticated, getStat]);
+
   const newName = useRef(null);
   return (
     <>
